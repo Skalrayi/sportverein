398c451a1abc679298160aaf40b3d04360c212e8
Christian Zahn <christian.zahn@bitzinger.de>
2022-05-17 11:48:17 +0200

    [TASK] styling login, adding form

--- www/index.php
+++ www/index.php
@@ -5,10 +5,26 @@
 ?>
 
 
-
-<div class="container frame login-background">
-    <h1>test</h1>
+<div class="container-fluid login-background">
+    <div class="container login-wrapper">
+        <div class="login-form-wrapper">
+            <h1 class="text-center">Sportverein</h1>
+            <h2 class="text-center">Login</h2>
+            <form action="" method="post">
+                <div class="mb-3">
+                    <label for="benutzername" class="form-label">Benutzername</label>
+                    <input type="text" class="form-control" id="benutzername">
+                </div>
+                <div class="mb-3">
+                    <label for="passwort" class="form-label">Passwort</label>
+                    <input type="password" class="form-control" id="passwort">
+                </div>
+                <button type="submit" class="btn btn-primary">Login</button>
+            </form>
+        </div>
+    </div>
 </div>
+
 
 <?php
     include "php/elements/footer.php"
