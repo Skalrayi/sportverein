1f8f71c85ccfb4db7b167f690bc94cb34cf7005f
Christian Zahn <christian.zahn@bitzinger.de>
2022-05-19 14:14:15 +0200

    [TASK] edit pages; general styling

---
+++ www/php/elements/modals.php
@@ -0,0 +1,137 @@
+<!-- Add Modal -->
+<div class="modal fade" id="addmember" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <div>
+                    <h2 class="modal-title" id="exampleModalLongTitle">Mitglied hinzufügen</h2>
+                    <hr>
+                </div>
+                <button type="button" class="close btn btn-light" data-bs-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form action="" method="post">
+                <div class="row mb-3">
+                    <div class="col-12 col-md-6">
+                        <label for="forename" class="form-label">Vorname *</label>
+                        <input type="text" class="form-control" id="forename" name="forename" required>
+                    </div>
+                    <div class="col-12 col-md-6">
+                        <label for="surname" class="form-label">Nachname *</label>
+                        <input type="text" class="form-control" id="surname" name="surname" required>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-12 col-md-6">
+                        <label for="zip" class="form-label">PLZ *</label>
+                        <input type="text" class="form-control" id="zip" name="zip" required>
+                    </div>
+                    <div class="col-12 col-md-6">
+                        <label for="city" class="form-label">Ort *</label>
+                        <input type="text" class="form-control" id="city" name="city" required>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-12 col-md-6">
+                        <label for="male" class="form-label">Geschlecht *</label>
+                        <select class="form-select" id="male" name="male"  required>
+                            <option selected>Geschlecht</option>
+                            <option value="male">Männlich</option>
+                            <option value="female">Weiblich</option>
+                        </select>
+                    </div>
+                    <div class="col-12 col-md-6">
+                        <label for="sport" class="form-label">Sportarten *</label>
+                        <select type="text" class="form-select mb-4" id="sport" name="sport" required>
+                            <option selected>Geschlecht</option>
+                            <option value="male">Männlich</option>
+                            <option value="female">Weiblich</option>
+                        </select>
+                        <button type="submit" class="btn btn-default submit-edit d-block w-100">Speichern</button>
+                    </div>
+
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<!-- Edit Modal -->
+<div class="modal fade" id="editmember" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <div>
+                    <h2 class="modal-title" id="exampleModalLongTitle">Mitglied bearbeiten</h2>
+                    <hr>
+                </div>
+                <button type="button" class="close btn btn-light" data-bs-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form action="" method="post">
+                <div class="row mb-3">
+                    <div class="col-12 col-md-6">
+                        <label for="forename" class="form-label">Vorname *</label>
+                        <input type="text" class="form-control" id="forename" name="forename" required>
+                    </div>
+                    <div class="col-12 col-md-6">
+                        <label for="surname" class="form-label">Nachname *</label>
+                        <input type="text" class="form-control" id="surname" name="surname" required>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-12 col-md-6">
+                        <label for="zip" class="form-label">PLZ *</label>
+                        <input type="text" class="form-control" id="zip" name="zip" required>
+                    </div>
+                    <div class="col-12 col-md-6">
+                        <label for="city" class="form-label">Ort *</label>
+                        <input type="text" class="form-control" id="city" name="city" required>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-12 col-md-6">
+                        <label for="male" class="form-label">Geschlecht *</label>
+                        <select class="form-select" id="male" name="male"  required>
+                            <option selected>Geschlecht</option>
+                            <option value="male">Männlich</option>
+                            <option value="female">Weiblich</option>
+                        </select>
+                    </div>
+                    <div class="col-12 col-md-6">
+                        <label for="sport" class="form-label">Sportarten *</label>
+                        <select type="text" class="form-select mb-4" id="sport" name="sport" required>
+                            <option selected>Geschlecht</option>
+                            <option value="male">Männlich</option>
+                            <option value="female">Weiblich</option>
+                        </select>
+                        <button type="submit" class="btn btn-default submit-edit d-block w-100">Speichern</button>
+                    </div>
+
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<!-- Delete Modal -->
+<div class="modal fade" id="deletemember" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <div>
+                    <h2 class="modal-title" id="exampleModalLongTitle">Mitglied wirklich löschen?</h2>
+                    <hr>
+                </div>
+                <button type="button" class="close btn btn-light" data-bs-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form action="" method="post">
+                <button type="submit" class="btn btn-default submit-delete d-block w-100">Löschen</button>
+            </form>
+        </div>
+    </div>
+</div>
--- www/php/pages/list.php
+++ www/php/pages/list.php
@@ -4,7 +4,7 @@ include __DIR__ . "/../elements/navbar.php";
 
 ?>
 
-<div class="container">
+<div class="container body-list">
     <div class="mb-5 mt-5">
         <h1>Mitgliederverwaltung</h1>
         <hr>
@@ -19,7 +19,7 @@ include __DIR__ . "/../elements/navbar.php";
 
     <div class="list container">
         <div class="row list-row list-head">
-            <div class="col id">#</div>
+            <div class="col id">ID</div>
             <div class="col surname">Nachname</div>
             <div class="col forename">Vorname</div>
             <div class="col zip">PLZ</div>
@@ -57,7 +57,15 @@ include __DIR__ . "/../elements/navbar.php";
             <div class="col male">Geschlecht</div>
             <div class="col sport">Sportart</div>
             <div class="col money">Grundbeitrag</div>
-            <div class="col edit">Bearbeiten</div>
+            <div class="col edit">
+                <button type="button" class="edit-button" data-bs-toggle="modal" data-bs-target="#editmember">
+                    <i class="fa fa-pencil" aria-hidden="true"></i>
+                </button>
+                <button type="button" class="delete-button" data-bs-toggle="modal" data-bs-target="#deletemember">
+                    <i class="fa fa-trash" aria-hidden="true"></i>
+                </button>
+
+            </div>
         </div>
         <div class="row list-row">
             <div class="col id">#</div>
@@ -68,153 +76,61 @@ include __DIR__ . "/../elements/navbar.php";
             <div class="col male">Geschlecht</div>
             <div class="col sport">Sportart</div>
             <div class="col money">Grundbeitrag</div>
-            <div class="col edit">Bearbeiten</div>
-        </div>
-        </tbody>
-    </div>
-    <!-- Button trigger modal -->
+            <div class="col edit">
+                <button type="button" class="edit-button" data-bs-toggle="modal" data-bs-target="#editmember">
+                    <i class="fa fa-pencil" aria-hidden="true"></i>
+                </button>
+                <button type="button" class="delete-button" data-bs-toggle="modal" data-bs-target="#deletemember">
+                    <i class="fa fa-trash" aria-hidden="true"></i>
+                </button>
 
-
-    <!-- Add Modal -->
-   <div class="modal fade" id="addmember" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <div>
-                        <h2 class="modal-title" id="exampleModalLongTitle">Mitglied hinzufügen</h2>
-                        <hr>
-                    </div>
-                    <button type="button" class="close btn btn-light" data-bs-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <form action="" method="post">
-                    <div class="row mb-3">
-                        <div class="col-6">
-                            <label for="forename" class="form-label">Vorname *</label>
-                            <input type="text" class="form-control" id="forename" name="forename" required>
-                        </div>
-                        <div class="col-6">
-                            <label for="surname" class="form-label">Nachname *</label>
-                            <input type="text" class="form-control" id="surname" name="surname" required>
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                        <div class="col-6">
-                            <label for="zip" class="form-label">PLZ *</label>
-                            <input type="text" class="form-control" id="zip" name="zip" required>
-                        </div>
-                        <div class="col-6">
-                            <label for="city" class="form-label">Ort *</label>
-                            <input type="text" class="form-control" id="city" name="city" required>
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                        <div class="col-6">
-                            <label for="male" class="form-label">Geschlecht *</label>
-                            <select class="form-select" id="male" name="male"  required>
-                                <option selected>Geschlecht</option>
-                                <option value="male">Männlich</option>
-                                <option value="female">Weiblich</option>
-                            </select>
-                        </div>
-                        <div class="col-6">
-                            <label for="sport" class="form-label">Sportarten *</label>
-                            <select type="text" class="form-select mb-4" id="sport" name="sport" required>
-                                <option selected>Geschlecht</option>
-                                <option value="male">Männlich</option>
-                                <option value="female">Weiblich</option>
-                            </select>
-                            <button type="submit" class="btn btn-default submit-edit d-block w-100">Speichern</button>
-                        </div>
-
-                    </div>
-                </form>
             </div>
         </div>
-    </div>
+        <div class="row list-row">
+            <div class="col id">#</div>
+            <div class="col surname">Nachname</div>
+            <div class="col forename">Vorname</div>
+            <div class="col zip">PLZ</div>
+            <div class="col city">Ort</div>
+            <div class="col male">Geschlecht</div>
+            <div class="col sport">Sportart</div>
+            <div class="col money">Grundbeitrag</div>
+            <div class="col edit">
+                <button type="button" class="edit-button" data-bs-toggle="modal" data-bs-target="#editmember">
+                    <i class="fa fa-pencil" aria-hidden="true"></i>
+                </button>
+                <button type="button" class="delete-button" data-bs-toggle="modal" data-bs-target="#deletemember">
+                    <i class="fa fa-trash" aria-hidden="true"></i>
+                </button>
 
-    <!-- Edit Modal -->
-   <div class="modal fade" id="editmember" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <div>
-                        <h2 class="modal-title" id="exampleModalLongTitle">Mitglied bearbeiten</h2>
-                        <hr>
-                    </div>
-                    <button type="button" class="close btn btn-light" data-bs-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <form action="" method="post">
-                    <div class="row mb-3">
-                        <div class="col-6">
-                            <label for="forename" class="form-label">Vorname *</label>
-                            <input type="text" class="form-control" id="forename" name="forename" required>
-                        </div>
-                        <div class="col-6">
-                            <label for="surname" class="form-label">Nachname *</label>
-                            <input type="text" class="form-control" id="surname" name="surname" required>
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                        <div class="col-6">
-                            <label for="zip" class="form-label">PLZ *</label>
-                            <input type="text" class="form-control" id="zip" name="zip" required>
-                        </div>
-                        <div class="col-6">
-                            <label for="city" class="form-label">Ort *</label>
-                            <input type="text" class="form-control" id="city" name="city" required>
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                        <div class="col-6">
-                            <label for="male" class="form-label">Geschlecht *</label>
-                            <select class="form-select" id="male" name="male"  required>
-                                <option selected>Geschlecht</option>
-                                <option value="male">Männlich</option>
-                                <option value="female">Weiblich</option>
-                            </select>
-                        </div>
-                        <div class="col-6">
-                            <label for="sport" class="form-label">Sportarten *</label>
-                            <select type="text" class="form-select mb-4" id="sport" name="sport" required>
-                                <option selected>Geschlecht</option>
-                                <option value="male">Männlich</option>
-                                <option value="female">Weiblich</option>
-                            </select>
-                            <button type="submit" class="btn btn-default submit-edit d-block w-100">Speichern</button>
-                        </div>
-
-                    </div>
-                </form>
             </div>
         </div>
-    </div>
-
-    <!-- Delete Modal -->
-    <div class="modal fade" id="deletemember" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <div>
-                        <h2 class="modal-title" id="exampleModalLongTitle">Mitglied wirklich löschen?</h2>
-                        <hr>
-                    </div>
-                    <button type="button" class="close btn btn-light" data-bs-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
+        <div class="row list-row list-pagination">
+            <div class="results">
+                <p>Ergebnisse pro Seite</p>
                 <form action="" method="post">
-                    <button type="submit" class="btn btn-default submit-delete d-block w-100">Löschen</button>
+                    <input type="number" class="form-control" placeholder="z.B. 20">
                 </form>
+                <p class="color-grey"> 1 - 20 von 120</p>
+            </div>
+            <div class="page">
+                <p>Aktuelle Seite</p>
+                <form action="" method="post">
+                    <input type="number" class="form-control" placeholder="z.B. 20">
+                </form>
+                <div class="arrows">
+                    <a href="#"><i class="fa fa-long-arrow-left" aria-hidden="true"></i></a>
+                    <a href="#"><i class="fa fa-arrow-left" aria-hidden="true"></i></a>
+                    <a href="#"><i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+                    <a href="#"><i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+                </div>
             </div>
         </div>
     </div>
 </div>
 
 <?php
+include __DIR__ . "/../elements/modals.php";
 include __DIR__ . "/../elements/footer.php"
 ?>
 
